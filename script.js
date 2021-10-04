42cf620f620f3af30bff7a9a9c2db254fe3cebfd
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-10-04 03:59:35 -0700

    wrote starter logic for capturing user input

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 // Assignment code here
+var charAlpha = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz']
+var charNumeric = ['1234567890']
+var charSpecial = ['!@#$%^&*()_+']
 
+var passRandom = charAlpha + charNumeric + charSpecial
+
+
+//Password user input questions
+var passGen = function(){
+  
+  var userInput = window.prompt('How long would you like your passowrd.');
+  
+
+  userInput = parseInt(userInput);
+ 
+
+  //If User input is less than or equal to 0
+  //If user input is less than or = to 7
+
+  while (userInput <= 0 || userInput <= 7){
+    window.alert('It is recommended to use a password that is more than 8 characters')
+    
+    passGen();
+}
+
+  
+  //If User input is greater than or equal to 8
+  //If user input is less than or 16 
+  if (userInput >= 8 || userInput <= 16){
+  
+    var userYesNo = window.prompt("Would you like to numbers in your password? Type 1 for yes 0 for no");
+    userYesNo = parseInt(userYesNo);
+
+    if (userYesNo == 1)
+    
+    // console.log(userYesNo);
+    
+
+    console.log(passRandom + " Here you go");
+  }
+
+  if (userYesNo == 0){
+    console.log(passRandom + " You can take the numbers out yourself")
+  }
+
+  else{
+    // console.log('Yo ish is broken Jose');
+    passGen();
+  } 
+  
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +65,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+passGen();
