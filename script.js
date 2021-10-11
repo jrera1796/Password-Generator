40e20a68f0de5043887c0c7de4eb441038490dc3
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-10-10 23:44:17 -0700

    Cleaned up code

--- script.js
+++ script.js
@@ -3,40 +3,32 @@ var charAlphaUpp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var charAlphaLow = "abcdefghijklmnopqrstuvwxyz"
 var charNumeric = "1234567890"
 var charSpecial = "!@#$%^&*()"
-var userYesNo="";
 var shortPassword = ""
-
-// var passRandom = charAlphaLow + charAlphaUpp + charNumeric + charSpecial
 
 //Password user input questions
 var generatePassword = function(){
+  //Resets symbols not clearing
   shortPassword = "";
-  var passLength = window.prompt('How long would you like your password?')
   
-
+  var passLength = window.prompt('How long would you like your password?')
   passLength = parseInt(passLength);
  
-
+  //I want the users password length stored in a variable, passLength
   //If passLength is less than 8
   //If passLength is greater than 128
 
   while (passLength < 8 || passLength > 128 || isNaN(passLength)){
     passLength = window.prompt('It is recommended to use a password that is more than 8 characters Please reenter a value between 8 - 128')
    
-    // generatePassword();
 }
-//I want the users password length stored in a variable, passLength
-
     var userYesNo = window.prompt("Would you like to numbers in your password? Type 1 for yes 0 for no");
     userYesNo = parseInt(userYesNo);
-
     var passRandom = ""
 
 if (userYesNo == 1){
   passRandom += charNumeric
-  // getRandomChars(passRandom, passLength);
-    
-  }
+}
+
 userYesNo = window.prompt("Do you want upper case letters? 1 for Yes 0 for No")
 
 if (userYesNo == 1){
@@ -47,7 +39,6 @@ userYesNo = window.prompt("Do you want lower case letters? 1 for Yes 0 for No")
 
 if (userYesNo == 1){
   passRandom += charAlphaLow
-  
 }
 
 userYesNo = window.prompt("Do you want special letters? 1 for Yes 0 for No")
@@ -59,9 +50,7 @@ if (userYesNo == 1){
   passLength--;
   passRandom+=charSpecial;
 }
-
   return (shortPassword + getRandomChars(passRandom,passLength));
-  
 };
 
 // Get references to the #generate element
@@ -88,10 +77,6 @@ function getRandomChars(passRandom, passLength){
 
   for(var i = 0; i < passLength; i++){
     string1 += passRandom[Math.floor(Math.random() * passRandom.length)]
-
-    /*Add an If statement to check if special characters were selected.
-    If special characters were selected but are not present
-    after running through Math.random run Math.random again until this equals true*/
 
   }
 
